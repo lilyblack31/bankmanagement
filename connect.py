a129bb1423d880077a51502bdc83ae07e2a718b5
lilyblack31 <63870252+lilyblack31@users.noreply.github.com>
2020-04-30 20:10:18 +0530

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,3 +1,3 @@
 import pymysql as sql
-db = sql.connect(host="localhost",port=3306,user="root",password="candus76",database="banking")
+db = sql.connect(host="localhost",port=3306,user="root",password="pwd",database="banking")
 c = db.cursor()
